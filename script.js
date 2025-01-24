8ebe0d5f5e951266f5a7ad60c6296fa3c1956dc7
Your name <Your email>
2025-01-24 16:12:09 +0100

    Initial commit for whatyearisit-frondend

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// fetch('http://localhost:3000/year') 
+//   .then(response => response.json()) 
+//   .then(data => {
+   
+//     const year = data.year;
+//     document.getElementById('year').textContent = year;  
+//   })
+//   .catch(error => {
+//     console.error('Erreur lors de la récupération de l\'année :', error);
+//     document.getElementById('year').textContent = 'Erreur de chargement';
+//   });
+  
+fetch('https://whatyearisit-backend-ashen-six.vercel.app')
+.then(response => response.json())
+.then(data => {
+  document.querySelector('#msg').textContent = data.message;
+});
+
